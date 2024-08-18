09485858aade634c58580a7bf987a652acd7a865
Ayush Patel <ayushptl1810@gmail.com>
2024-08-18 21:41:59 +0530

    Completed backend

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ class Student(db.Model):
     name = db.Column(db.String(64), nullable=False)
     tag = db.Column(db.String(16), nullable=False)
     dept = db.Column(db.String(16), nullable=False)
+    selected_flag = db.Column(db.Boolean, default = False)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    results = Student.query.filter_by(selected_flag = True).all()
+    return render_template('home.html', results=results)
 
 @app.route('/add_student')
 def add_student():
@@ -34,12 +36,76 @@ def add_student_post():
     dept = request.form.get('dept')
 
     if not name or not tag or not dept:
-        flash('Fill all fields')
         return redirect(url_for('add_student'))
     
     new_student = Student(name=name, tag=tag, dept=dept)
     db.session.add(new_student)
     db.session.commit()
 
-    flash('Student successfully added')
     return redirect(url_for('add_student'))
+
+@app.route('/filter')
+def filter():
+    return render_template('filter.html')
+
+@app.route('/filter', methods=['POST'])
+def filter_post():
+    query = Student.query
+    filters = []
+    
+    name = request.form.get('name')
+    tags = request.form.getlist('year_tag')
+    depts = request.form.getlist('dept')
+
+    if name:
+        filters.append(Student.name.like(f"%{name}%"))
+    if tags:
+        filters.append(Student.tag.in_(tags)) 
+    if depts:
+        filters.append(Student.dept.in_(depts))
+
+    filters.append(Student.selected_flag == False)
+    query = query.filter(*filters)
+
+    results = query.all()
+    return render_template('filter.html', results=results, name=name, tag=tags, dept=depts)
+
+@app.route('/add_filter', methods=['POST'])
+def add_filter():
+    student_id = request.form.get('id')
+    student = Student.query.get(student_id)
+    student.selected_flag = True
+    db.session.commit()
+
+    name = request.form.get('name')
+    tag = request.form.get('year_tag')
+    dept = request.form.get('dept')
+
+    query = Student.query
+    filters = []
+
+    if name:
+        filters.append(Student.name.like(f"%{name}%"))
+    if tag:
+        tags = tag.split(',')
+        filters.append(Student.tag.in_(tags))
+    if dept:
+        depts = dept.split(',')
+        filters.append(Student.dept.in_(depts))
+
+    filters.append(Student.selected_flag == False)
+    query = query.filter(*filters)
+
+    results = query.all()
+
+    return render_template('filter.html', results=results, name=name, tag=tag, dept=dept)
+
+
+@app.route('/remove', methods = ['POST'])
+def remove():
+    student_id = request.form.get('id')
+    student = Student.query.get(student_id)
+    student.selected_flag = False
+    db.session.commit()
+
+    return redirect(url_for('home'))
--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import TableExtractor as te
 import TableLinesRemover as tlr
 import cv2
 
-path_to_image = "image/Sample.png"
-table_extractor = te.TableExtractor(path_to_image)
-perspective_corrected_image = table_extractor.execute()
-cv2.imshow("perspective_corrected_image", perspective_corrected_image)
+def process_image():
+    path_to_image = "image/Sample.png"
+    table_extractor = te.TableExtractor(path_to_image)
+    perspective_corrected_image = table_extractor.execute()
+    cv2.imshow("perspective_corrected_image", perspective_corrected_image)
 
 
-lines_remover = tlr.TableLinesRemover(perspective_corrected_image)
-image_without_lines = lines_remover.execute()
-cv2.imshow("image_without_lines", image_without_lines)
+    lines_remover = tlr.TableLinesRemover(perspective_corrected_image)
+    image_without_lines = lines_remover.execute()
+    cv2.imshow("image_without_lines", image_without_lines)
 
-ocr_tool = ottt.OcrToTableTool(image_without_lines, perspective_corrected_image)
-ocr_tool.execute()
+    ocr_tool = ottt.OcrToTableTool(image_without_lines, perspective_corrected_image)
+    ocr_tool.execute()
 
-cv2.waitKey(10000)
-cv2.destroyAllWindows()
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
